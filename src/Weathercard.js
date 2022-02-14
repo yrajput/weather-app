48c25d69011d31287354d24c22a54513b49a9235
MaxK <max.kleinerman@perficient.com>
2022-02-14 11:33:53 -0600

    style sheets

--- src/Weathercard.js
+++ src/Weathercard.js
@@ -1,24 +1,32 @@
 import React, {Component} from 'react'
+import './Weathercard.css';
 
 
 class Weathercard extends Component {
     state = {
         days: [
-          {name: 'Monday', temp: '79', forecast: 'cloudy'},
-          {name: 'Tuesday', temp: '79', forecast: 'cloudy'},
-          {name: 'Wednesday', temp: '79', forecast: 'cloudy'},
-          {name: 'Thursday', temp: '79', forecast: 'cloudy'},
-          {name: 'Friday', temp: '79', forecast: 'cloudy'},
+          {name: 'Monday', date: 'March 1st, 1:00 pm', temp: '79', forecast: 'cloudy'},
+          {name: 'Tuesday', date: 'March 2nd, 1:00 pm',temp: '79', forecast: 'cloudy'},
+          {name: 'Wednesday', date: 'March 3rd, 1:00 pm',temp: '79', forecast: 'cloudy'},
+          {name: 'Thursday', date: 'March 4th, 1:00 pm',temp: '79', forecast: 'cloudy'},
+          {name: 'Friday', date: 'March 5th, 1:00 pm',temp: '79', forecast: 'cloudy'},
         ],
+        location: 'Boise, Idaho',
       }
   render() {
       
     return (
       <div>
-        <ul className="WeatherList">
+        <div className="Location">{this.state.location}</div>
+        <div className="WeatherList">
           {this.state.days.map((day) =>
-            <div key={day.name}>{day.name} {day.temp} {day.forecast}</div>)}
-        </ul>
+            <div className="IndividualCard">
+              <div className="DayName"> {day.name} </div>
+              <div className="DayDate"> {day.date} </div>
+              <div className="DayTemp"> {day.temp} </div>
+              <div className="DayForecast"> {day.forecast} </div>
+              </div>)}
+        </div>
       </div>
     )
   }
