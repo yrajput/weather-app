d25e1e7d2560571688268b28f8d363d8f422ccb8
MaxK <max.kleinerman@perficient.com>
2022-02-16 11:55:26 -0600

    Implemented Prettier extension on all files

--- src/Weathercard.js
+++ src/Weathercard.js
@@ -1,12 +1,17 @@
-import React, { useEffect,useState } from 'react'
-import './Weathercard.css';
-import { useDispatch, useSelector } from 'react-redux'
-import { getWeather, setLocation, setDays, setSelectedDay, setHourly, getHourlyWeather, getCoords } from './weather';
-
-
+import React, { useEffect, useState } from "react";
+import "./Weathercard.css";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getWeather,
+  setLocation,
+  setDays,
+  setSelectedDay,
+  setHourly,
+  getHourlyWeather,
+  getCoords,
+} from "./weather";
 
 export default function Weathercard() {
-
   const dispatch = useDispatch();
 
   const { days, location, selectedDay, coords } = useSelector((state) => {
@@ -15,40 +20,54 @@ export default function Weathercard() {
       location: state.location,
       selectedDay: state.selectedDay,
       coords: state.coords,
-    }
-  })
+    };
+  });
   const [newLocation, setNewLocation] = useState(location);
   const changeLocation = (event) => {
     setNewLocation(event.target.value);
-  }
+  };
 
   useEffect(() => {
-    dispatch(getWeather())
-  }, [coords])
+    dispatch(getWeather());
+  }, [coords]);
 
   useEffect(() => {
-    dispatch(getHourlyWeather())
-  }, [selectedDay])
+    dispatch(getHourlyWeather());
+  }, [selectedDay]);
 
   return (
     <div>
       <div className="Location">{location}</div>
-      <input type="text" name="location" value={newLocation}
-               onChange={changeLocation}/>
-        <button type="button" onClick={() => {dispatch(setLocation(newLocation)); dispatch(getCoords())}}>Submit</button>
+      <input
+        type="text"
+        name="location"
+        value={newLocation}
+        onChange={changeLocation}
+      />
+      <button
+        type="button"
+        onClick={() => {
+          dispatch(setLocation(newLocation));
+          dispatch(getCoords());
+        }}
+      >
+        Submit
+      </button>
       <div className="WeatherList">
-
-        {(days.slice(0, 5)).map((day) =>
-          <div className="IndividualCard" key={day.date} onClick={() => dispatch(setSelectedDay(day.id))}>
+        {days.slice(0, 5).map((day) => (
+          <div
+            className="IndividualCard"
+            key={day.date}
+            onClick={() => dispatch(setSelectedDay(day.id))}
+          >
             <div className="DayName"> {day.name} </div>
             <div className="DayDate"> {day.date} </div>
             <div className="DayTemp"> {day.temp} &#x2109; </div>
             <div className="DayForecast"> {day.forecast} </div>
-            <img src= {day.img}></img>
-          </div>)}
+            <img src={day.img}></img>
+          </div>
+        ))}
       </div>
     </div>
-  )
+  );
 }
-
-
--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import store from './redux';
-import { Provider } from 'react-redux'
-import { getCoords, getHourlyWeather, getWeather } from './weather';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import store from "./redux";
+import { Provider } from "react-redux";
+import { getCoords, getHourlyWeather, getWeather } from "./weather";
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-store.dispatch(getWeather, getHourlyWeather, getCoords)
+store.dispatch(getWeather, getHourlyWeather, getCoords);
